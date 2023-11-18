2b55bacd820238e3e624f98e76810d51d0454154
Noorazam8 <135502888+Noorazam8@users.noreply.github.com>
2023-11-18 19:18:45 +0500

    Add files via upload
    ARRAY AND LOOP

--- app.js
+++ app.js
@@ -1,35 +1,82 @@
-alert("welcome to js land. \nHappy coding");
+// var emptyArray =[];
+// document.write(emptyArray)
+// var matrixArray = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1],
+// ]
+// console.log(matrixArray)
 
-var userName = "Muhammad";
-var myName = "Noor azam";
-console.log(myName)
+// 3. Write a program to print numeric counting from 1 to 10.
+// for(i=1; i<=10; i++ ){
+//     document.write( i + "<br>")
+// }
 
-var message = "hello world";
-alert(message)
+// var num = prompt("Enter the number");
+// var length = prompt("Enter the length")
+// for(i=1; i<length ; i++){
+//     document.write(num + "X" + i + "="+ i*num + "<br>" )
+// }
 
-// bio data of studunts
-var studentName = "harry";
-var address = "Islamabad";
-var rollNo =20005;
-var age = 15;
-// result of student
-alert("Name:" + studentName );
-alert("ADDRESS:" + address);
-alert("ROLLNO:" + rollNo);
-alert("AGE:" + age);
-//  variable alert
-alert("PIZZA \n PIZZ\n PIZ\n PI \n P");
-// alert of email
+// var fruit = [ "apple","mango" ,"banana" , "strawberry","orange"];
+// for(i=0 ; i<fruit.length; i++){
+//     document.write("Element of index" + i + " \tis \t"+ fruit[i]  + "<br>")
+// } 
 
-var email = "noorazam@gmail.com"
-alert( "MY EMAIL ADDRESS:"+ email);
-var string ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
-alert(string);
-// visitor online clothing store
-var visitorName = "john doe";
-var productTitle = "t-shirt";
-var quantity = 5;
-// result
+// for(i=1; i<=15; i++){
+//     document.write(  i  +","  )
+// }
 
-var result = visitorName +"ordered"+  quantity + productTitle 
-console.log("result");
+// for(i=10; i>=0; i--){
+//     document.write( i + ",")
+// }
+// for(var i = 0; i <= 20; i+=2){
+//     document.write(i + ",");
+// }
+
+// Task 7
+// var arr = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var order = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
+// var match = "no";
+
+// for(var i = 0; i < arr.length; i++){
+//     if(arr[i] == order){
+//         match = "yes";
+//         break;
+//     }
+// }
+// if(match == "yes"){
+//     document.write(order + " is available in our bakery");
+// }
+// else{
+//     document.write(order + " isn't available in our bakery");
+// }
+
+// Task 8
+// var arr = [24, 53, 78, 91, 12];
+// document.write("<h2>" + "Array items: " + arr + "</h2>");
+
+// var smallestNumber = arr[0];
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] < smallestNumber) {
+//     smallestNumber = arr[i];
+//   }
+// }
+
+// document.write("<h2>" + "The smallest number is: ", smallestNumber + "</h2>");
+
+// Task 9
+// var arr = [24, 53, 78, 91, 12];
+// document.write("<h2>" + "Array items: " + arr + "</h2>");
+
+// var largestNumber = arr[0];
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] > largestNumber) {
+//     largestNumber = arr[i];
+//   }
+// }
+// document.write("<h2>" + "The largest number is: ", largestNumber + "</h2>");
+
+
